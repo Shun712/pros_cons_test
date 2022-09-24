800978957129ecad842e036787b74cad2acbb6ca
Shun712 <sh705vi@gmail.com>
2022-09-24 22:29:58 +0900

    ⑤ミルククラウン領域のみを、cv2.imshow()を用いて表示

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from unittest import result
 import cv2
 import numpy as np
 
@@ -9,21 +10,21 @@ cv2.destroyAllWindows()
 
 # ②白色領域(ミルククラウン)とそれ以外の2値画像を作成
 img_binary = cv2.imread('milkdrop.bmp', 0)
-threshold = 135
+threshold = 134
 ret, img_thresh = cv2.threshold(img_binary, threshold, 255, cv2.THRESH_BINARY)
-cv2.imshow("img_th", img_thresh)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 # ③作成した2値画像をもとに、白色領域（ミルククラウン）の輪郭を抽出
 contours = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 contour = max(contours, key=lambda x: cv2.contourArea(x))
 
 # ④抽出した領域を用いて、ミルククラウン領域とそれ以外を分けたmask画像を作成
-out = np.zeros_like(img_thresh)
-mask = cv2.drawContours(out, [contour], -1, color=255, thickness=-1)
-result = np.where(mask==255, img_thresh, out)
-# img_contour = cv2.drawContours(img_thresh, contours, -1, (255, 0, 0), 3)
-cv2.imshow("img_edge", result)
+mask = np.zeros_like(img_thresh)
+result = cv2.drawContours(mask, [contour], -1, color=255, thickness=-1)
+cv2.imwrite("mask.bmp", result)
+
+# ⑤ミルククラウン領域のみを、cv2.imshow()を用いて表示
+img_mask = cv2.imread("mask.bmp")
+milkcrown = cv2.bitwise_and(img_origin, img_mask)
+cv2.imshow("img_th", milkcrown)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
